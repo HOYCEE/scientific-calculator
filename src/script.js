02760e32e4e1aa75ec5e85db0ee5afc984457870
HOYCEE <158208993+HOYCEE@users.noreply.github.com>
2024-02-26 02:59:56 +0000

    implemented a few scientific fuctions

--- src/script.js
+++ src/script.js
@@ -4,28 +4,52 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.getElementsByClassName('btn');
 
     let currentValue = "";
-  
+  function evaluateResult() {
+   console.log('currentValue;',currentValue)
+   const convertedValue = currentValue
+   .replace("×","*")
+   .replace("÷","/")
+   .replace("%","*0.01")
+   .replace("sin","math.sin")
+   .replace("cos","math.cos")
+   .replace('ln', 'math.ln')
+   .replace('π',"math.PI");
+   replace('log',"math.log")
+   .replace('e',"math.e")
+   .replace('tan','math.tan')
+   .replace("√'",'math.sqrt')
+}
   
   function evaluateResult() {
     const result = eval(currentValue);
    currentValue = result.toString();
    display.value = currentValue;
+
 }
 
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
 
-            if (value == "AC") {
-               currentValue= "";
-               display.value=currentValue
-             } else if (value == "=") {
-                evaluateResult();
-             }else {
-                currentValue += Value
-                display.value = currentValue
-             }
-            })
+        try{
+         
+         if ('value' == "AC") {
+            currentValue= "";
+            display.value=currentValue
+          } else if ('value' == "=") {
+             evaluateResult();
+          }else {
+             currentValue = value
+             display.value = currentValue 
+          }
+        }catch (error) {
+console.error(error);
+currentValue= "ERROR"
+display.value = currentValue
+        }
+
+            
+      })
     }
 
 });
